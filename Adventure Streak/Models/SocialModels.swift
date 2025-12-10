4d0125ab22cecd00b2742c85f29eee40c09ba325
Anyelo Reyes Guevara <aerguevara@gmail.com>
2025-12-10 07:45:08 +0100

    Permitir activityData sin campos nuevos en feed (defaults a 0)

--- Adventure Streak/Models/SocialModels.swift
+++ Adventure Streak/Models/SocialModels.swift
@@ -26,7 +26,32 @@ struct SocialActivityData: Codable, Hashable {
     let defendedZonesCount: Int
     let recapturedZonesCount: Int
     
+    enum CodingKeys: String, CodingKey {
+        case activityType, distanceMeters, durationSeconds, xpEarned, newZonesCount, defendedZonesCount, recapturedZonesCount
+    }
+    
     var distanceKm: Double {
         distanceMeters / 1000.0
+    }
+    
+    init(activityType: ActivityType, distanceMeters: Double, durationSeconds: Double, xpEarned: Int, newZonesCount: Int, defendedZonesCount: Int, recapturedZonesCount: Int) {
+        self.activityType = activityType
+        self.distanceMeters = distanceMeters
+        self.durationSeconds = durationSeconds
+        self.xpEarned = xpEarned
+        self.newZonesCount = newZonesCount
+        self.defendedZonesCount = defendedZonesCount
+        self.recapturedZonesCount = recapturedZonesCount
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        activityType = try container.decode(ActivityType.self, forKey: .activityType)
+        distanceMeters = try container.decode(Double.self, forKey: .distanceMeters)
+        durationSeconds = try container.decode(Double.self, forKey: .durationSeconds)
+        xpEarned = try container.decode(Int.self, forKey: .xpEarned)
+        newZonesCount = try container.decodeIfPresent(Int.self, forKey: .newZonesCount) ?? 0
+        defendedZonesCount = try container.decodeIfPresent(Int.self, forKey: .defendedZonesCount) ?? 0
+        recapturedZonesCount = try container.decodeIfPresent(Int.self, forKey: .recapturedZonesCount) ?? 0
     }
 }
